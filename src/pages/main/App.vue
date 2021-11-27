f3ed3d85a73b8fa95e4e8aa0962c15a3dd6c419d
kuangch <kuangch_8017@qq.com>
2021-11-27 12:40:46 +0800

    基本框架

--- src/pages/main/App.vue
+++ src/pages/main/App.vue
@@ -1,13 +1,70 @@
 <template>
     <div id="app">
-        <header>
-            <img src="../../assets/logo.png"/>
-        </header>
-        <div class="main">
-            <div class="content">
-                <hello-world/>
+      <div class="header">
+        <div class="title">
+          <div class="back">
+            <convenience-image :src-nor="back" alignment='max-contain'></convenience-image>
+          </div>
+          <div class="jkb">北京健康宝</div>
+          <div class="right">
+            <div class="more">
+              <convenience-image :src-nor="more" alignment='max-contain'></convenience-image>
             </div>
+            <div class="divider"></div>
+            <div class="close">
+              <convenience-image :src-nor="close" alignment='max-contain'></convenience-image>
+            </div>
+          </div>
         </div>
+      </div>
+      <div class="over">
+        <div class="main">
+          <div class="time">
+            <div class="left">
+              <convenience-image :src-nor="bj" alignment='max-contain'></convenience-image>
+            </div>
+            <div class="rt">
+              <p class="day">{{realDay}}</p>
+              <p class="time" style="margin-top: 5px">{{realTime}}</p>
+            </div>
+            <div class="right">
+              <convenience-image :src-nor="sm" alignment='max-contain'></convenience-image>
+            </div>
+          </div>
+          <div class="person">
+            <div class="pic">
+              <img :src="hulei">
+            </div>
+          </div>
+          <div class="result">
+            <div class="icon">
+              <convenience-image :src-nor="duihao" alignment='max-contain'></convenience-image>
+            </div>
+            <p class="txt">未见异常</p>
+            <div class="info">
+              <convenience-image :src-nor="wenhao" alignment='max-contain'></convenience-image>
+            </div>
+          </div>
+          <div class="item">
+            <span class="name">姓 名:</span>
+            <span class="value">户*</span>
+          </div>
+          <div class="item">
+            <span class="name">身份证号:</span>
+            <span class="value">37**************99</span>
+          </div>
+          <div class="item">
+            <span class="name">查询时间:</span>
+            <span class="value">{{qtime}}</span>
+          </div>
+          <div class="item">
+            <span class="name">失效时间:</span>
+            <span class="value" style="color: #e90009">{{ltime}} 24:00</span>
+          </div>
+        </div>
+        <div class="back">返回首页</div>
+        <div class="info">北京市大数据中心</div>
+      </div>
 
     </div>
 </template>
@@ -15,6 +72,11 @@
 <script>
 
     const packageJson = require('../../../package')
+    const wh = require('../../assets/wenhao.png')
+    const dh = require('../../assets/duihao.png')
+    const bk = require('../../assets/back.png')
+    const cl = require('../../assets/close.png')
+    const mo = require('../../assets/more.png')
 
     import HelloWorld from '../../components/HelloWorld.vue'
 
@@ -23,11 +85,63 @@
         components: {
             HelloWorld
         },
-        mounted: function () {
+        created: function () {
+
+          /**************************************时间格式化处理************************************/
+          function dateFtt(fmt,date)
+          { //author: meizz
+            var o = {
+              "M+" : date.getMonth()+1,                 //月份
+              "d+" : date.getDate(),                    //日
+              "h+" : date.getHours(),                   //小时
+              "m+" : date.getMinutes(),                 //分
+              "s+" : date.getSeconds(),                 //秒
+              "q+" : Math.floor((date.getMonth()+3)/3), //季度
+              "S"  : date.getMilliseconds()             //毫秒
+            };
+            if(/(y+)/.test(fmt))
+              fmt=fmt.replace(RegExp.$1, (date.getFullYear()+"").substr(4 - RegExp.$1.length));
+            for(var k in o)
+              if(new RegExp("("+ k +")").test(fmt))
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+            return fmt;
+          }
+
+          let _this = this;
+          let date = new Date();
+
+
+          _this.qtime = dateFtt("MM-dd hh:mm", date);
+          _this.ltime = dateFtt("MM-dd", date);
+          _this.realDay = dateFtt("yyyy年MM月dd日", date);
+          _this.realTime = dateFtt("hh:mm:ss", date);
+
+          setInterval(function (){
+
+            let date = new Date();
+
+            _this.realTime = dateFtt("hh:mm:ss", date);
+
+
+          }, 1000);
+        },
+        beforeDestroy() {
         },
         data() {
             return {
                 packageJson,
+              qtime: "11-27 09:20",
+              ltime: "11-27",
+              realDay: "2021年11月26日",
+              realTime: "10:20:10",
+              wenhao: wh,
+              duihao: dh,
+              close: cl,
+              back: bk,
+              more: mo,
+              bj: require('../../assets/bj.jpeg'),
+              sm: require('../../assets/sm.jpeg'),
+              hulei: require('../../assets/hulei.gif'),
             }
         },
         methods: {}
@@ -36,32 +150,193 @@
 </script>
 
 <style lang="scss" scoped>
-    $header_content_height: 2rem;
+    $header_content_height: 5rem;
+    $header_icon_size: 25px;
 
     #app {
+        position: relative;
+        background: #f7f7f7;
         min-height: 100vh;
 
-        header {
-            height: $header_content_height * 1.5;
+        > .header {
+          border-radius: 0 ;
+          padding-top: 25px;
+          width: 100%;
+            height: 120px;
             background: $color_main;
+          > .title{
+            padding-left: 20px;
+            padding-right: 10px;
             display: flex;
-            justify-content: flex-start;
             align-items: center;
-
-            color: $color_main_txt;
-
-            img {
-                margin-left: .8vw;
-                height: $header_content_height;
+            > .back{
+              width: $header_icon_size;
+              height: $header_icon_size;
             }
-            .app-name {
-                margin-left: 1vw;
-                font-size: calc(1rem + 6px);
-                font-weight: bold;
+            > .jkb{
+              color: white;
+              font-size: 20px;
             }
+            > .right{
+              height: fit-content;
+              background: #00000050;
+              margin-left: auto;
+              border-radius: 50px;
+              border: 0.5px solid gray;
+              padding: 2px 12px;
+              > .more{
+                display: inline-block;
+                width: $header_icon_size;
+                height: $header_icon_size;
+              }
+              > .divider{
+                margin-left: 12px;
+                margin-right: 12px;
+                display: inline-block;
+                background: #ffffff50;
+                width: 0.5px;
+                height: $header_icon_size - 5px;;
+              }
+              > .close{
+                display: inline-block;
+                width: $header_icon_size;
+                height: $header_icon_size;
+              }
+            }
+          }
+
 
         }
-        > .main {
+        > .over {
+          position: absolute;
+          left: 50%;
+          top: 0;
+          transform: translate(-50%, 0);
+
+          margin-top: 80px;
+          width: 90%;
+          z-index: 10;
+
+          > .main{
+            position: relative;
+            border-radius: 10px;
+            text-align: center;
+            padding-top: 12px;
+            padding-bottom: 25px;
+            color: white;
+
+            font-size: 16px;
+            background: white;
+
+            > .time{
+              width: 100%;
+              $icon_size : 50px;
+              display: flex;
+              align-items: center;
+              > .left{
+                margin-left: 10px;
+                width: $icon_size;
+                height: $icon_size;
+              }
+              > .rt{
+                flex: 1;
+                color: grey;
+                font-size: 18px;
+                text-align: center;
+              }
+              > .right{
+                margin-left: auto;
+                width: $icon_size;
+                height: $icon_size;
+              }
+            }
+            > .person{
+              width: fit-content;
+              margin: 15px auto;
+
+
+              > .pic{
+                //border: 10px dotted #6600d7 ;
+                $size: 200px;
+                width: $size;
+                height: $size;
+                position: relative;
+
+                > img{
+                  width: auto !important;
+                  height: auto !important;
+                  position: absolute;
+                  max-width: 100% !important;
+                  max-height: 100% !important;
+                  left: 0;
+                  top: 0;
+                  right: 0;
+                  bottom: 0;
+                  margin: 0 auto;
+                  border-radius: 5px;
+                }
+              }
+            }
+            > .result{
+              width: fit-content;
+              margin: 0 auto;
+              display: flex;
+              align-items: center;
+              > .icon{
+                width: 50px;
+                height: 50px;
+              }
+              > .txt{
+                margin-left: 8px;
+                display: inline-block;
+                font-size: 30px;
+                font-weight: bolder;
+                color: #1cc709;
+              }
+              > .info{
+                margin-left: 8px;
+                display: inline-block;
+                width: 25px;
+                height: 25px;
+              }
+            }
+            > .item{
+              width: 100%;
+              display: flex;
+              margin-top: 18px;
+              > .name{
+                margin-left: 15px;
+                letter-spacing: 2px;
+                word-spacing: 30px;
+                float: left;
+                color: #b5b5b5;
+              }
+              > .value{
+                margin-left: auto;
+                margin-right: 15px;
+                color: black;
+              }
+            }
+          }
+
+          > .back{
+            margin-top: 20px;
+            border-radius: 8px;
+            text-align: center;
+            padding-top: 12px;
+            padding-bottom: 10px;
+            color: white;
+            width: 100%;
+            font-size: 20px;
+            background: #1292fe;
+          }
+
+          > .info{
+            margin-top: 20px;
+            text-align: center;
+              height: 12px;
+              color: #c5c5c5;
+          }
 
         }
     }
